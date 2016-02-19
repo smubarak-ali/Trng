bd765e6fd5052d1e82d79ae8676c87c6b3e7571f
Mubarak Ali <mubarakali.00@gmail.com>
2016-02-19 12:56:02 +0800

    Removed the useless files.

--- SecureWCF/Tranglo.Auth.Service.Api/Custom/TokenValidationInspector.cs
+++ SecureWCF/Tranglo.Auth.Service.Api/Custom/TokenValidationInspector.cs
@@ -10,34 +10,34 @@ using System.Web.Services.Description;
 
 namespace Tranglo.Auth.Service.Api.Custom
 {
-    public class TokenValidationInspector : IDispatchMessageInspector
+    public class TokenValidationInspector //: IDispatchMessageInspector
     {
-        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
-        {
-            // Return BadRequest if request is null
-            if (WebOperationContext.Current == null) { throw new WebFaultException(HttpStatusCode.BadRequest); }
+        //public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        //{
+        //    // Return BadRequest if request is null
+        //    if (WebOperationContext.Current == null) { throw new WebFaultException(HttpStatusCode.BadRequest); }
 
-            // Get Token from header
-            var token = WebOperationContext.Current.IncomingRequest.Headers["Token"];
+        //    // Get Token from header
+        //    var token = WebOperationContext.Current.IncomingRequest.Headers["Token"];
 
-            // Validate the Token
-            //using (var dbContext = new BasicTokenDbContext())
-            {
-                //ITokenValidator validator = new DatabaseTokenValidator(dbContext);
-                //if (!validator.IsValid(token))
-                //{
-                //    throw new WebFaultException(HttpStatusCode.Forbidden);
-                //}
-                // Add User ids to the header so the service has them if needed
-                WebOperationContext.Current.IncomingRequest.Headers.Add("User", "User......");
-                WebOperationContext.Current.IncomingRequest.Headers.Add("UserId", "UserId.......");
-            }
-            return null;
-        }
+        //    // Validate the Token
+        //    //using (var dbContext = new BasicTokenDbContext())
+        //    {
+        //        //ITokenValidator validator = new DatabaseTokenValidator(dbContext);
+        //        //if (!validator.IsValid(token))
+        //        //{
+        //        //    throw new WebFaultException(HttpStatusCode.Forbidden);
+        //        //}
+        //        // Add User ids to the header so the service has them if needed
+        //        WebOperationContext.Current.IncomingRequest.Headers.Add("User", "User......");
+        //        WebOperationContext.Current.IncomingRequest.Headers.Add("UserId", "UserId.......");
+        //    }
+        //    return null;
+        //}
 
-        public void BeforeSendReply(ref Message reply, object correlationState)
-        {
-        }
+        //public void BeforeSendReply(ref Message reply, object correlationState)
+        //{
+        //}
     }
     
 }
--- SecureWCF/Tranglo.Auth.Service.Api/Custom/TokenValidationServiceBehavior.cs
+++ SecureWCF/Tranglo.Auth.Service.Api/Custom/TokenValidationServiceBehavior.cs
@@ -9,30 +9,30 @@ using System.Web;
 
 namespace Tranglo.Auth.Service.Api.Custom
 {
-    public class TokenValidationServiceBehavior : IServiceBehavior
+    public class TokenValidationServiceBehavior //: IServiceBehavior
     {
-        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
-        {
-        }
+        //public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
+        //{
+        //}
 
-        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-            foreach (var t in serviceHostBase.ChannelDispatchers)
-            {
-                var channelDispatcher = t as ChannelDispatcher;
-                if (channelDispatcher != null)
-                {
-                    foreach (var endpointDispatcher in channelDispatcher.Endpoints)
-                    {
-                        endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new TokenValidationInspector());
-                    }
-                }
-            }
-        }
+        //public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        //{
+        //    foreach (var t in serviceHostBase.ChannelDispatchers)
+        //    {
+        //        var channelDispatcher = t as ChannelDispatcher;
+        //        if (channelDispatcher != null)
+        //        {
+        //            foreach (var endpointDispatcher in channelDispatcher.Endpoints)
+        //            {
+        //                endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new TokenValidationInspector());
+        //            }
+        //        }
+        //    }
+        //}
 
-        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
-        {
-        }
+        //public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
+        //{
+        //}
     }
     
 }
